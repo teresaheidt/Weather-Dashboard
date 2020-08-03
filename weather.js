62927035640970334df350fc78ecf7028efc60e6
Teresa Heidt <teresaheidt@hotmail.com>
2020-08-03 17:17:26 -0500

    added weather

--- weather.js
+++ weather.js
@@ -1,42 +1,81 @@
 // city array
-var cityArray = ['Chicago', 'New York', 'Orlando', 'San Francisco', 'Seattle', 'Denver', 'Atlanta',]
+$(document).ready(function(){
+var history = ['Chicago', 'New York', 'Orlando', 'San Francisco', 'Denver', 'Atlanta',];
 
-$(".city").on("click", function(event) {
-    event.preventDefault();
+for(var i = 0; i < history.length; i ++ ) {
+   makeRow(history[i])
+}
 
+function makeRow(text) {
+   var li = $("<li>").addClass("list-group-item").text(text)
+
+   $(".history").append(li)
+}
+
+$("#city-search").on("click", function() {
+   var city = $("#add-city").val();
+   console.log(city)
+displayWeatherInfo(city)
+}) 
 // display the weather content
-function displayWeatherInfo() {
+function displayWeatherInfo(city) {
 
-var APIKey = 'd2edc2080024ef0841b4893641476d0a';     
-var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=Dallas&appid=d2edc2080024ef0841b4893641476d0a" + APIKey;     
+   if(history.indexOf(city) === -1) {
+   history.push(city);
+   makeRow(history[i])
+   }
+
+var APIKey = 'd2edc2080024ef0841b4893641476d0a';  
+// 5-Day forecast URL   
+var queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKey}&units=imperial`
 
 // ajax call for the city being clicked
 $.ajax({
     url: queryURL,
-    method: 'GET'
-}).then(function(response) {
-   console.log(response)
+    method: 'GET',
+    dataType: "json"
+})
 
-// TEMPERATURE (retrieve, element, set text, append)
-   var temperature = $('<p>');
-   temperatureEl.text('Temperature: ' + temperature);
-   temperatureDiv.append(temperatureEl);
-   console.log(temperature)
-// HUMIDITY (retrieve, element, set text, append)
-   var humidity = $('<p>');
-   humidityEl.text('Humidity: ' + humidity);
-   humidityDiv.append(humidityEl);
-// WIND SPEED (retrieve, element, set text, append)
-   var windSpeed = $('<p>');
-   windSpeedEl.text('Wind Speed: ' + windSpeed);
-   windSpeedDiv.append(windSpeedEl);
-// UV INDEX (retrieve, element, set text, append)
-   var uvIndex = $('<p>');
-   uvIndexEl.text('UV Index: ' + uvIndex);
-   uvIndexDiv.append(uvIndexEl);
+.then(function(response) {
+   console.log(response)
+   //  cityArray.text(JSON.stringify(response));
+   $("#today").empty();
+
+var card = $("<div>").addClass("card-city");
+var cardBody = $("<div>").addClass("card-body");
+var cardTitle = $("<h3>").addClass("card-title").text(response.name);
+var temp = $("<h5>").addClass("card-text").text("Tempature: " + response.main.temp);
+var wind = $("<h5>").addClass("card-text").text("Wind speed: " + response.wind.speed);
+var humidity = $("<h5>").addClass("card-text").text("Humidity: " + response.main.humidity);
+
+$("#today").append(card.append(cardBody.append(cardTitle, temp, wind, humidity)))
+
+getforecast(response.coord.lat, response.coord.lon);
 
 });    
+}
 
-$(document).on("click", ".city", displayCityInfo);
+function getforecast(lat, lon) {
+   var APIKey = 'd2edc2080024ef0841b4893641476d0a';  
+// 5-Day forecast URL   
+var queryURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${APIKey}&units=imperial`
 
-});
+// ajax call for the city being clicked
+$.ajax({
+    url: queryURL,
+    method: 'GET',
+    dataType: "json"
+}).then(function(res){
+ 
+   //for loop over res.daily.length
+for (i=0; i<forecast.length; i++) {
+   forecast[i].innerHTML = "";
+   console.log(res)
+
+   
+}
+
+   
+})
+}
+})
